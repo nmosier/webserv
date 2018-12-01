0f3a4a92e180806751ad10397da6ffbec1a5b59c
Nicholas Mosier <nmosier@middlebury.edu>
2018-12-01 15:27:04 -0500

    now tests functionality of lib

--- webserv-multi.c
+++ webserv-multi.c
@@ -4,18 +4,43 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <pthread.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #include "webserv-lib.h"
 #include "webserv-multi.h"
 
 int main(int argc, char *argv[]) {
    /* test server_start() */
-   int sock_fd;
-   if ((sock_fd = server_start(argv[1], BACKLOG)) < 0) {
+   int server_fd;
+   if ((server_fd = server_start(argv[1], BACKLOG)) < 0) {
       fprintf(stderr, "server_start failed.\n");
       exit(1);
    }
 
-   printf("sock_fd=%d\n", sock_fd);
+   printf("server_fd=%d\n", server_fd);
+   
+   /* accept connection */
+   struct sockaddr_in client_sa;
+   socklen_t addrlen;
+   int client_fd;
+   
+   do {
+      addrlen = sizeof(client_sa);
+      client_fd = accept(server_fd, (struct sockaddr *) &client_sa, &addrlen);
+   } while (client_fd < 0 && (errno == EAGAIN || errno == EWOULDBLOCK));
+   
+   printf("client_fd=%d\n", client_fd);
+
+   /* get HTTP request */
+   httpreq_t *req = NULL; // must initialize
+   int req_stat;
+   do {
+      req_stat = request_read(server_fd, client_fd, &req);
+   } while (req_stat == REQ_RD_RAGAIN);
+
+   printf("request_read status: %d\n", req_stat);
    
    exit(0);
 }
