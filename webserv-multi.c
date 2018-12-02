173a99426aa7e6f2926fb348b3cc61d40e6857cc
Nicholas Mosier <nmosier@middlebury.edu>
2018-12-02 11:12:00 -0500

    prints out contents of headers

--- webserv-multi.c
+++ webserv-multi.c
@@ -34,13 +34,42 @@ int main(int argc, char *argv[]) {
    printf("client_fd=%d\n", client_fd);
 
    /* get HTTP request */
-   httpreq_t *req = NULL; // must initialize
+   httpreq_t *req = malloc(sizeof(httpreq_t));
+   if (req == NULL) {
+      perror("malloc");
+      exit(1);
+   }
+   if (request_init(req) < 0) {
+      perror("request_init");
+      exit(2);
+   }
+   
    int req_stat;
    do {
-      req_stat = request_get(server_fd, client_fd, &req);
+      req_stat = request_read(server_fd, client_fd, req);
    } while (req_stat == REQ_RD_RAGAIN);
 
    printf("request_read status: %d\n", req_stat);
+   if (req_stat == REQ_RD_RSUCCESS) {
+      printf("HTTP REQUEST:\n%s\n", req->hr_text);
+   }
+
+   /* parse request */
+   int parse_stat = request_parse(req);
+   printf("request_parse status: %d\n", parse_stat);
+   if (parse_stat == REQ_PRS_RSUCCESS) {
+      printf("mode = %d, URI = %s, version = %s\n", req->hr_line.method,
+             req->hr_line.uri, req->hr_line.version);
+   }
+
+   /* print out headers*/
+   printf("HEADERS\n");
+   for (httpreq_header_t *header_it = req->hr_headers;
+        header_it->key; ++header_it) {
+      printf("%s:\t%s\n", header_it->key, header_it->value);
+   }
+
+   request_delete(req);
    
    exit(0);
 }
