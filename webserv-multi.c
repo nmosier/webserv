b073c81fce2229df9312ded2c9a1a4ac7a3e0df8
Nicholas Mosier <nmosier@middlebury.edu>
2018-12-09 17:03:15 -0500

    changed to use different lib prototypes

--- webserv-multi.c
+++ webserv-multi.c
@@ -54,55 +54,55 @@ int main(int argc, char *argv[]) {
    printf("client_fd=%d\n", client_fd);
 
    /* get HTTP request */
-   httpmsg_t *req;
-   if ((req = message_init()) == NULL) {
+   httpmsg_t req;
+   if (message_init(&req) < 0) {
       perror("message_init");
       exit(2);
    }
    
    int req_stat;
    do {
-      req_stat = request_read(server_fd, client_fd, req);
+      req_stat = request_read(server_fd, client_fd, &req);
    } while (req_stat == REQ_RD_RAGAIN);
 
    printf("request_read status: %d\n", req_stat);
    if (req_stat == REQ_RD_RSUCCESS) {
-      printf("HTTP REQUEST:\n%s\n", req->hm_text);
+      printf("HTTP REQUEST:\n%s\n", req.hm_text);
    }
 
    /* parse request */
-   int parse_stat = request_parse(req);
+   int parse_stat = request_parse(&req);
    printf("request_parse status: %d\n", parse_stat);
    if (parse_stat == REQ_PRS_RSUCCESS) {
-      printf("mode = %d, URI = %s, version = %s\n", req->hm_line.reql.method,
-             HM_OFF2STR(req->hm_line.reql.uri, req), HM_OFF2STR(req->hm_line.reql.version, req));
+      printf("mode = %d, URI = %s, version = %s\n", req.hm_line.reql.method,
+             HM_OFF2STR(req.hm_line.reql.uri, &req), HM_OFF2STR(req.hm_line.reql.version, &req));
    }
 
    /* print out headers*/
    printf("HEADERS\n");
-   for (httpmsg_header_t *header_it = req->hm_headers;
+   for (httpmsg_header_t *header_it = req.hm_headers;
         header_it->key; ++header_it) {
-      printf("%s:\t%s\n", HM_OFF2STR(header_it->key, req), HM_OFF2STR(header_it->value, req));
+      printf("%s:\t%s\n", HM_OFF2STR(header_it->key, &req), HM_OFF2STR(header_it->value, &req));
    }
 
-   httpmsg_t *res;
-   if ((res = message_init()) == NULL) {
+   httpmsg_t res;
+   if (message_init(&res) < 0) {
       perror("message_init");
       exit(3);
    }
 
-   if (response_insert_line(C_FORBIDDEN, "1.1", res) < 0) {
+   if (response_insert_line(C_FORBIDDEN, "1.1", &res) < 0) {
       perror("response_insert_line");
       exit(4);
    }
 
-   if (server_handle_req(client_fd, DOCUMENT_ROOT, SERVER_NAME, req) < 0) {
+   if (server_handle_req(client_fd, DOCUMENT_ROOT, SERVER_NAME, &req) < 0) {
       perror("server_handle_get");
       exit(7);
    }
 
-   message_delete(req);
-   message_delete(res);
+   message_delete(&req);
+   message_delete(&res);
    close(client_fd);
    
    exit(0);
