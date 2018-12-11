10676f760a63dad409cdbf1d4e7836d3e657471c
Nicholas Mosier <nmosier@middlebury.edu>
2018-12-11 14:24:19 -0500

    initial commit

--- webserv-multi.h
+++ webserv-multi.h
@@ -1,2 +1,14 @@
-#define BACKLOG 10
-#define SERVER_PORT 30000
+#ifndef __WEBSERV_MULTI_H
+#define __WEBSERV_MULTI_H
+
+/* types */
+struct client_thread_args {
+   int client_fd;
+};
+
+/* prototypes */
+void *client_loop(void *args);
+int server_loop(int servfd);
+
+
+#endif
